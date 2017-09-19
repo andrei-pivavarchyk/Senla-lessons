3e162b9801c51c062ecd418c22b8c3bdf15e2981
Hellsing\Andrey Hrumov <andreyatake666@mail.ru>
2017-09-19 23:02:31 +0300

    task2

---
+++ Lesson3/Task2/src/Doctor.java
@@ -0,0 +1,20 @@
+class Doctor extends Man
+{
+
+
+
+    public void setName(String name) {
+
+    }
+
+    public String getName() {
+        return null;
+    }
+    public void setSurName(String surName) {
+
+    }
+
+    public String getSurName() {
+        return null;
+    }
+}
---
+++ Lesson3/Task2/src/Man.java
@@ -0,0 +1,13 @@
+public abstract class Man {
+    private String name;
+    private String surName;
+
+
+
+    public abstract void setName(String name);
+    public abstract String getName();
+
+    public abstract void setSurName(String surName);
+    public abstract String getSurName();
+
+}
