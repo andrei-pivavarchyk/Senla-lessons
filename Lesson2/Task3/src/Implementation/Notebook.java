05977356b600fc6da8dd0bcd760cf2749a7d8746
Hellsing\Andrey Hrumov <andreyatake666@mail.ru>
2017-09-23 10:56:49 +0300

    task3

--- Lesson2/Task3/src/Implementation/Notebook.java
+++ Lesson2/Task3/src/Implementation/Notebook.java
@@ -4,21 +4,27 @@ import Contracts.IProductPart;
 
 public class Notebook implements IProduct{
 
+    private IProductPart firstPart;
+    private  IProductPart secondPart;
+    private IProductPart thirdPart;
 
 
-    public void InstallFirstPart(IProductPart FirstProductPart) {
+
+    public void InstallFirstPart(IProductPart firstProductPart) {
+        this.firstPart=firstProductPart;
 
     System.out.println("First Part installed");
     }
 
-    public void InstallSecondPart(IProductPart SecondProductPart)
+    public void InstallSecondPart(IProductPart secondProductPart)
     {
-
+        this.secondPart=secondProductPart;
         System.out.println("Second Part installed");
     }
 
-    public void InstallThirdPart(IProductPart ThirdProductPart)
+    public void InstallThirdPart(IProductPart thirdProductPart)
     {
+        this.thirdPart=thirdProductPart;
 
         System.out.println("Third Part installed");
     }
