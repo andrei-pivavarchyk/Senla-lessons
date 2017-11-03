db635f983072829ac513b49f8a1aa2f594e0a341
Andrey <andreyatake666@mail.ru>
2017-11-03 16:28:34 +0200

    no message

--- Lesson7/ui/Task/src/com/hotelInterface/Main.java
+++ Lesson7/ui/Task/src/com/hotelInterface/Main.java
@@ -8,6 +8,7 @@ public class Main {
 
     public static void main(String[] args) {
         StartHotelService startHotelService=new StartHotelService();
+        startHotelService.startHotel();
         MenuController menuController = new MenuController();
         menuController.run();
     }
