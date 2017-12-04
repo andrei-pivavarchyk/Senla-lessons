5a74c1c2f7beb64b7bc2f2b8278d942a9fa746f4
Andrey <andreyatake666@mail.ru>
2017-12-04 15:50:26 +0200

    no message

--- Lesson10/Task2/task.sql
+++ Lesson10/Task2/task.sql
@@ -1,12 +1,14 @@
 use somedevices;
 
 
-
+/*Задание 1*/
 SELECT model,speed,hd
 
 FROM pc
 
 WHERE price>500;
+
+/*Задание 2*/
 
 SELECT maker
 
@@ -14,15 +16,21 @@ FROM product
 
 WHERE type='printer';
 
+/*Задание 3*/
+
 SELECT model,hd, screen
 
 FROM laptop
 
 WHERE price>1000;
 
+/*Задание 4*/
+
 SELECT*FROM printer
 
 WHERE color='y';
+
+/*Задание 5*/
 
 SELECT model,speed,hd
 
@@ -30,12 +38,8 @@ FROM pc
 
 WHERE cd='12x' OR cd='24x';
 
-SELECT model,speed,hd
 
-FROM pc
-
-WHERE (cd='4x' OR cd='5x') AND price<600;
-
+/*Задание 6*/
 SELECT maker,type,speed
 
 FROM product,laptop
@@ -139,6 +143,70 @@ FROM pc
 group by hd
 
 HAVING COUNT(model)>=2
+
+/*Задание 16*/
+
+SELECT DISTINCT A.model, B.model, A.speed,A.ram
+FROM pc AS A,pc AS B
+WHERE A.speed=B.speed AND A.ram=B.ram AND A.model>B.model
+
+/*Задание 17*/
+SELECT DISTINCT product.type,laptop.model,laptop.speed
+FROM product,laptop
+WHERE laptop.speed<ANY
+(SELECT MIN(speed) FROM pc)
+AND product.model=laptop.model
+
+/*Задание 18*/
+SELECT DISTINCT maker,price
+FROM product,printer
+WHERE printer.color='y'
+AND product.model=printer.model
+AND printer.price=
+(SELECT MIN(price)
+FROM printer
+WHERE color='y'
+)
+
+/*Задание 19*/
+SELECT maker,AVG(screen)
+FROM product
+INNER JOIN
+laptop 
+ON laptop.model=product.model
+GROUP BY maker
+
+/*Задание 20*/
+SELECT maker, COUNT(model)
+FROM product
+WHERE product.type='pc'
+GROUP BY maker
+HAVING COUNT(DISTINCT model)>=3
+
+
+/*Задание 21*/
+SELECT product.maker,MAX(pc.price)
+FROM product,pc
+WHERE product.model=pc.model
+GROUP BY maker
+
+
+/*Задание 22*/
+SELECT speed,AVG(price)
+FROM pc
+WHERE speed>600
+GROUP BY speed
+
+/*Задание 23*/
+SELECT maker
+FROM product,pc
+WHERE pc.speed>=750 AND product.model=pc.model
+INTERSECT
+SELECT maker
+FROM product,laptop
+WHERE laptop.speed>=750 AND product.model=laptop.model
+
+/*Задание 24*/
 
 
 
