0d9c8fb8008ce1d5b63bcd568a85a4b02b883fe9
Andrey <andreyatake666@mail.ru>
2018-01-31 17:12:42 +0200

    bootstrap

--- Lesson16/client/webapp/src/app/app.module.ts
+++ Lesson16/client/webapp/src/app/app.module.ts
@@ -1,18 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes/heroes.component';
+import { AppNavbarComponent } from './app-navbar/app-navbar.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    AppNavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+     NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
