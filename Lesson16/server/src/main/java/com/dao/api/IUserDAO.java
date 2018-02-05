9d6324191abda28ccf00b64de0eca12ef60b2669
Hellsing\Andrey Hrumov <andreyatake666@mail.ru>
2018-02-05 20:42:26 +0300

    no message

--- Lesson16/server/src/main/java/com/dao/api/IUserDAO.java
+++
@@ -1,8 +0,0 @@
-package com.dao.api;
-
-import com.entity.User;
-
-public interface IUserDAO extends IBaseDAO<User> {
-    Boolean checkUser(User user);
-    User getUserByLoginPassword(String login,String password);
-}
--- Lesson16/server/src/main/java/com/dao/api/IUserDataDAO.java
+++
@@ -1,8 +0,0 @@
-package com.dao.api;
-
-import com.entity.User;
-import com.entity.UserData;
-
-public interface IUserDataDAO extends IBaseDAO<UserData> {
-    UserData getDataByUser(User user);
-}
